4f65fa8a59bcce2f20e74d69df99f1c4fcd1bc79
Danny <danny.costelloe@gmail.com>
2021-05-05 20:34:25 -0400

    done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,23 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  def email_list 
+    readers_arr = self.readers
+    emails_arr = readers_arr.map {|reader_instance| reader_instance.email}
+    emails_arr.join(';')
+  end
+
+  def self.most_popular
+    most_popular_magazine = Magazine.first
+
+    Magazine.all.each do |mag_instance|
+        if mag_instance.subscriptions.count > most_popular_magazine.subscriptions.count
+            most_popular_magazine = mag_instance
+        end
+    end
+    most_popular_magazine
+  end
+
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,19 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine_instance, price_int)
+        Subscription.create(magazine_id: magazine_instance.id, reader_id: self.id, price: price_int)
+    end
+
+    def total_subscription_price
+        self.subscriptions.sum(:price)
+    end
+
+    def cancel_subscription(mag_instance)
+        sub = Subscription.find_by(magazine_id: mag_instance, reader_id: self.id)
+        sub.destroy
+    end
+
+
 end
